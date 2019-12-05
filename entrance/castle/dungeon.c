e93b5c97f24c6e96ffc47f95f84be4167f8a45a2
mgonzalez28 <31522535+mgonzalez28@users.noreply.github.com>
2019-12-05 11:42:44 -0600

    Working on dungeon - basic idea down but need to come up with story/instructions

--- entrance/castle/dungeon.c
+++ entrance/castle/dungeon.c
@@ -2,19 +2,44 @@
 #include <unistd.h>
 
 int main() {
-    if (access("./bridge", R_OK) != -1) {
-		FILE *filepointer;
-		filepointer = fopen("../notepad.txt", "a");
-		fprintf(filepointer, "%d %c\n", 2, 82);
-		fclose(filepointer);
-		printf("You found another clue in the prisoner note!\n");
-		printf("Clue printed to notepad!");
-	}
+    
+    int a = 294; 
+    int b = 999;
+    int c = 698;
 
-    else {
-		printf("You need to change user persmissions to read the note!");
-	}
+    int code_sum = a + b + c;
+
+    while(code_sum != 1991) {
+        printf("Wrong code!\n");
+        sleep(2);
+
+    }
+
+    FILE *filepointer;
+    filepointer = fopen("../notepad.txt", "a");
+    fprintf(filepointer, "%d %c\n", 2, 82);
+    fclose(filepointer);
+    printf("You found another clue!\n");
+    printf("Clue printed to notepad!");
+	
 	
 	return 1;
 
 }
+
+
+/*
+*Story: you enter into a castle but as you walk in the drawbridge you took enter the castle compund
+*closes behind you. You make your way to the dungeon and find a prisoner note. The note contains the 
+*code needed to lower the draw bridge but the note is very lengthy and makes no sense. You notice the prisoner 
+*left the following hint for you to make it easier to find the code: "code for the drawbridge labeled 'BRIDGE_CODE'"
+*Successful retrieval of the code will result in discovery of another clue. It is your job to initialize the variables a, b , and c
+* 
+*
+*Have a sum and player has to find 3 double digit numbers in the note that gives them that sum
+*
+*TIP: grep is a command used to find literal strings in a file but be careful because grep is a case-sensative command
+*
+*Draw bridge and to get the code they must use the grep to search the file
+*
+*/
